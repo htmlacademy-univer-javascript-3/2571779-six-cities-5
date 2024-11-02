4ba90dc30eb58e2fc14810847f017909dbfb5474
bolthen <92172378+bolthen@users.noreply.github.com>
2024-11-02 18:07:06 +0500

    Вынес Header в отдельный компонент

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import {AppRoute} from '../app-route.ts';
-import {Link} from 'react-router-dom';
+import {Header} from '../components/header.tsx';
 
 export const LoginPage: React.FC = () => (
   <div className="page page--gray page--login">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <Link className="header__logo-link" to={AppRoute.Main}>
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </header>
+    <Header addNavigation={false}/>
 
     <main className="page__main page__main--login">
       <div className="page__login-container container">
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {OffersList} from './offers-list.tsx';
 import {AppRoute} from '../../app-route.ts';
 import {OfferShortInfo} from '../../models/offer-short-info.ts';
+import {Header} from "../../components/header.tsx";
 
 interface IMainPageProps {
   offers: OfferShortInfo[];
@@ -10,34 +11,7 @@ interface IMainPageProps {
 
 export const MainPage: React.FC<IMainPageProps> = ({offers}) => (
   <div className="page page--gray page--main">
-    <header className="header">
-      <div className="container">
-        <div className="header__wrapper">
-          <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </a>
-          </div>
-          <nav className="header__nav">
-            <ul className="header__nav-list">
-              <li className="header__nav-item user">
-                <a className="header__nav-link header__nav-link--profile" href="#">
-                  <div className="header__avatar-wrapper user__avatar-wrapper">
-                  </div>
-                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                  <Link className="header__favorite-count" to={AppRoute.Favorites}>3</Link>
-                </a>
-              </li>
-              <li className="header__nav-item">
-                <a className="header__nav-link" href="#">
-                  <span className="header__signout">Sign out</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
+    <Header addNavigation isLogoActive/>
 
     <main className="page__main page__main--index">
       <h1 className="visually-hidden">Cities</h1>
