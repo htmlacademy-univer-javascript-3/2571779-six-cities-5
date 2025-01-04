8faad63c835611b06df2e6eff65ce1297142cd01
klementev.m <klementev.m@skbkontur.ru>
2025-01-04 23:43:58 +0500

    фикс багов

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,6 +9,7 @@ export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
+      serializableCheck: false,
       thunk: {
         extraArgument: api,
       },
