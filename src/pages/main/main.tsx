e79ed2437986609c0ea14bf5dc382d7c257bbb7b
klementev.m <klementev.m@skbkontur.ru>
2025-01-04 18:09:07 +0500

    добавил отображение страницы если нет предложений по аренде

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -24,25 +24,40 @@ export const MainPage: React.FC = () => {
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <CitiesList cities={CITIES}/>
+        {!isDataLoaded && <Spinner/>}
         {isDataLoaded && (
           <div className="cities">
             <div className="cities__places-container container">
-              <section className="cities__places places">
-                <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{sortedOffers.length} places to stay in {city.name}</b>
-                <SortOptions/>
-                <OffersList
-                  offers={sortedOffers}
-                  setActiveOffer={setActiveCard}
-                />
-              </section>
+              {sortedOffers && sortedOffers.length > 0 && (
+                <section className="cities__places places">
+                  <h2 className="visually-hidden">Places</h2>
+                  <b className="places__found">{sortedOffers.length} places to stay in {city.name}</b>
+                  <SortOptions/>
+                  <OffersList
+                    offers={sortedOffers}
+                    setActiveOffer={setActiveCard}
+                  />
+                </section>
+              )}
+              {(!sortedOffers || sortedOffers.length === 0) && (
+                <div className="cities__places-container--empty">
+                  <section className="cities__no-places">
+                    <div className="cities__status-wrapper tabs__content">
+                      <b className="cities__status">No places to stay available</b>
+                      <p className="cities__status-description">
+                        We could not find any property available at the moment in {city.name}
+                      </p>
+                    </div>
+                  </section>
+                  <div className="cities__right-section"></div>
+                </div>
+              )}
               <div className="cities__right-section">
-                <Map city={city} offersLocation={sortedOffers} activeLocationId={activeCard?.id}/>
+                <Map city={city} offersLocation={sortedOffers ?? []} activeLocationId={activeCard?.id}/>
               </div>
             </div>
           </div>
         )}
-        {!isDataLoaded && <Spinner/>}
       </main>
     </div>
   );
